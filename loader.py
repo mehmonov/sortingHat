37cd78d6e8c5227085ecad931c2ace6d7b8ec73b
Husniddin <uzbekfreedom@gmail.com>
2023-11-18 21:39:26 +0500

    token

--- loader.py
+++ loader.py
@@ -5,5 +5,5 @@ from models.database.sqlite import Database
 
 
 config: Config = load_config()
-bot: Bot = Bot(token="6462565189:AAGWj75nUp4n97FDK-0WuTyLJb-Tn65kW84", parse_mode="HTML")
+bot: Bot = Bot(token="6371651493:AAHDDFwX6V-i16Cc1Lbi8uD3gwdyVMnW9VA", parse_mode="HTML")
 db = Database(path_to_db="main.db")
